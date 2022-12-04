6f44b0237bdd70d6516749b66570c69525375f68
Luis <lumaro@disroot.org>
2022-12-04 15:31:23 +0100

    update obmenu-generator config

--- virootfs/etc/skel/.config/obmenu-generator/config.pl
+++ virootfs/etc/skel/.config/obmenu-generator/config.pl
@@ -24,8 +24,8 @@
                             ],
 
 || ICON SETTINGS
-    | use_gtk3            : Use the Gtk3 library for resolving the icon paths. (default: 0)
-    | gtk_rc_filename     : Absolute path to the GTK configuration file.
+    | gtk_version         : The version of the Gtk library used for resolving the icon paths. (default: 3)
+    | gtk_rc_filename     : Absolute path to the Gtk configuration file.   
     | missing_icon        : Use this icon for missing icons (default: gtk-missing-image)
     | icon_size           : Preferred size for icons. (default: 48)
     | generic_fallback    : Try to shorten icon name at '-' characters before looking at inherited themes. (default: 0)
@@ -50,7 +50,8 @@ our $CONFIG = {
   "editor"              => "geany",
   "force_icon_size"     => 0,
   "generic_fallback"    => 0,
-  "gtk_rc_filename"     => "$ENV{HOME}/.gtkrc-2.0",
+  "gtk_rc_filename"     => "$ENV{HOME}/.config/gtk-3.0/settings.ini",
+  "gtk_version"         => 3,
   "icon_size"           => 48,
   "Linux::DesktopFiles" => {
                              desktop_files_paths     => [
@@ -70,6 +71,5 @@ our $CONFIG = {
   "locale_support"      => 1,
   "missing_icon"        => "gtk-missing-image",
   "terminal"            => "xterm",
-  "use_gtk3"            => 0,
-  "VERSION"             => 0.89,
+  "VERSION"             => 0.91,
 }
