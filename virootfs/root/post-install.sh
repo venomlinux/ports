951bf0b85dd416ee27087ff7912485d92b59c451
emmett1 <emmett1.2miligrams@protonmail.com>
2023-03-17 00:44:23 +0800

    updated new venom installer

--- virootfs/root/post-install.sh
+++ virootfs/root/post-install.sh
@@ -1,38 +1,90 @@
 #!/bin/sh
 #
-# this script will be execute by venom-installer to do some stuffs
-# after venom is installed
+# this script execute to apply changes to installed system from data from venom-installer
 #
- 
-VIROOTFS=${VIROOTFS:-/run/initramfs/medium/virootfs/}
-VENOMROOTFS=${VENOMROOTFS:-/tmp/venominstall}
- 
-# copy over all customs configs and customization except some unnecessary files
-for i in $(find $VIROOTFS -type f | sed "s,$VIROOTFS,,"); do
-    case $i in
-        *venom-installer*|*custom_script.sh|*fstab|*issue|*venominstaller.desktop|*post-install.sh) continue;;
-    esac
-    install -D $VIROOTFS/$i $VENOMROOTFS/$i
+# list variable:
+#  $ROOT - directory where system is installed
+#  $HOSTNAME   - hostname
+#  $TIMEZONE   - timezone (xxxx/yyyy)
+#  $KEYMAP     - keymap
+#  $USERNAME   - user's login name
+#  $USER_PSWD  - user's password
+#  $ROOT_PSWD  - root's password
+#  $LOCALE     - locale
+#  $BOOTLOADER - disk to install grub (either '/dev/sdX or skip)
+#  $EFI_SYSTEM - 1 if boot in UEFI mode
+#
+
+if [ -x $ROOT/etc/rc.d/networkmanager ]; then
+	network=networkmanager
+elif [ -x $ROOT/etc/rc.d/network ]; then
+	network=network
+fi
+
+if [ -x $ROOT/etc/rc.d/lxdm ]; then
+	dm=lxdm
+elif [ -x $ROOT/etc/rc.d/lightdm ]; then
+	dm=lightdm
+elif [ -x $ROOT/etc/rc.d/sddm ]; then
+	dm=sddm
+elif [ -x $ROOT/etc/rc.d/slim ]; then
+	dm=slim
+fi
+
+daemons="sysklogd dbus $dm alsa bluetooth gpm $network"
+
+# enable services
+for d in $daemons; do
+	if [ -x $ROOT/etc/rc.d/$d ]; then
+		if [ "$dd" ]; then
+			dd="$dd $d"
+		else
+			dd="$d"
+		fi
+	fi
 done
 
-#xchroot $VENOMROOTFS sh /root/customization.sh
-# skels not automatically copied over when user created through chroot
-# so use this for now
-#for user in $VENOMROOTFS/home/*; do
-	#USER=${user##*/}
-	#cp -r /etc/skel/.* /etc/skel/* $VENOMROOTFS/home/$USER
-	#chown -R $USER:$USER /home/$USER/.* $VENOMROOTFS/home/$USER/*
-#done
+# hostname
+echo "$HOSTNAME" > $ROOT/etc/hostname
 
-# remove install venom entry from ob menu
-sed '/Install Venom/d' -i $VENOMROOTFS/etc/skel/.config/obmenu-generator/schema.pl
+# hardware clock
+#sed "s;#HARDWARECLOCK=.*;HARDWARECLOCK=\"$clock_var\";" -i $ROOT/etc/rc.conf
 
-# remove welcome message entry from autostart
-sed '/xterm/d' -i $VENOMROOTFS/etc/skel/.config/openbox/autostart
+# timezone
+sed "s;#TIMEZONE=.*;TIMEZONE=\"$TIMEZONE\";" -i $ROOT/etc/rc.conf
 
-# change slim theme
-[ -d /usr/share/slim/themes/greeny_dark ] && {
-	sed "s/current_theme.*/current_theme greeny_dark/" -i $VENOMROOTFS/etc/slim.conf
-}
+# keymap
+sed "s;#KEYMAP=.*;KEYMAP=\"$KEYMAP\";" -i $ROOT/etc/rc.conf
 
-exit 0
+# daemons
+sed "s;#DAEMONS=.*;DAEMONS=\"$dd\";" -i $ROOT/etc/rc.conf
+
+# create user
+# 'useradd -R' not copy all skel files, use xchroot instead
+#useradd -R $ROOT -m -G users,wheel,audio,video -s /bin/bash $USERNAME
+xchroot $ROOT useradd -m -G users,wheel,audio,video -s /bin/bash $USERNAME
+echo "$USERNAME:$USER_PSWD" | chpasswd -R $ROOT -c SHA512
+
+# root pswd
+echo "root:$ROOT_PSWD" | chpasswd -R $ROOT -c SHA512
+
+# locale
+sed "s/#$LOCALE/$LOCALE/" -i $ROOT/etc/locales
+echo "LANG=$LOCALE.UTF-8" > $ROOT/etc/locale.conf
+xchroot $ROOT genlocales
+
+# initramfs
+xchroot $ROOT mkinitramfs
+
+# grub
+if [ "$BOOTLOADER" != skip ]; then
+	echo GRUB_DISABLE_OS_PROBER=false >> $ROOT/etc/default/grub
+	if [ "$EFI_SYSTEM" = 1 ]; then
+		# EFI
+		xchroot $ROOT grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=venom_grub --recheck $BOOTLOADER
+	else
+		# mbr
+		xchroot $ROOT grub-install --target=i386-pc $BOOTLOADER
+	fi
+	xchroot $ROOT grub-mkconfig -o /boot/grub/grub.cfg
+fi
