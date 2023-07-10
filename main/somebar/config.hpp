3cd6bf444f592bc2f4bf4751dc0dfc621ebd02d7
Luis <lumaro@disroot.org>
2023-07-10 09:12:40 +0000

    somebar: new port

---
+++ main/somebar/config.hpp
@@ -0,0 +1,27 @@
+// somebar - dwl bar
+// See LICENSE file for copyright and license details.
+
+#pragma once
+#include "common.hpp"
+
+constexpr bool topbar = true;
+
+constexpr int paddingX = 10;
+constexpr int paddingY = 3;
+
+// See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
+constexpr const char* font = "Sans 12";
+
+constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
+constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
+constexpr const char* termcmd[] = {"foot", nullptr};
+
+static std::vector<std::string> tagNames = {
+	"1", "2", "3",
+	"4", "5", "6",
+	"7", "8", "9",
+};
+
+constexpr Button buttons[] = {
+	{ ClkStatusText,   BTN_RIGHT,  spawn,      {.v = termcmd} },
+};
