8515602603510ab03a3d0a66d3a2d1f0f1f0c063
Luis <98406209+lumarogit@users.noreply.github.com>
2023-10-08 16:51:09 +0200

    build.sh: update release to 20231008

--- scripts/build.sh
+++ scripts/build.sh
@@ -490,7 +490,7 @@ SCRIPTDIR="$(dirname $(realpath $0))"
 parse_opts "$@"
 
 ARCH=$(uname -m)
-RELEASE=20230903
+RELEASE=20231008
 
 TARBALLIMG="$PORTSDIR/venomlinux-rootfs-$ARCH.tar.xz"
 SRCDIR="${SRCDIR:-/var/cache/scratchpkg/sources}"
